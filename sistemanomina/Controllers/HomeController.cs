acc60e93482ccf37d4ddce55cb14cece34738eef
abudsystem <abudsystem@gmail.com>
2025-08-16 16:53:19 -0500

    Add project files.

---
+++ sistemanomina/Controllers/HomeController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sistemanomina.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        // Aqui es todo lo creado
+        public ActionResult Autenticacion()
+        {
+            ViewBag.Message = "Pagina de Autenticacion";
+            return View();
+        }
+        public ActionResult Empleados()
+        {
+            ViewBag.Message = "Pagina de Empleados";
+            return View();
+        }
+        public ActionResult Departamentos()
+        {
+            ViewBag.Message = "Pagina de Departamentos";
+            return View();
+        }
+        public ActionResult AsignacionADepartamentos()
+        {
+            ViewBag.Message = "Pagina de asignacion a departamentos";
+            return View();
+        }
+        public ActionResult GerentesDeDepartamento()
+        {
+            ViewBag.Message = "Pagina de los Gerentes de departamento";
+            return View();
+
+        
+        }
+        public ActionResult TitulosCargos()
+        {
+            ViewBag.Message = "Pagina de los ttulos / Cargos";
+            return View();
+
+
+        }
+        public ActionResult Salarios()
+        {
+            ViewBag.Message = "Pagina de Salarios";
+            return View();
+
+
+        }
+        public ActionResult Reportes()
+        {
+            ViewBag.Message = "Pagina de los Reportes";
+            return View();
+
+
+        }
+        public ActionResult Administracion()
+        {
+            ViewBag.Message = "Pagina de Administracion";
+            return View();
+
+
+        }
+    }
+}
---
+++ sistemanomina/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="sistemanomina.MvcApplication" Language="C#" %>
