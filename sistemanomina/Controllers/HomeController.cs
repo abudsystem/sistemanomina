c3c365373a91bab6932cc437818662e62f2d9b3c
abudsystem <abudsystem@gmail.com>
2025-08-18 10:46:13 -0500

    Prueba de Entity con el uso de Store procedures . Ejemplificando para la presentacion de 1 empleado concreto

--- sistemanomina/Controllers/HomeController.cs
+++ sistemanomina/Controllers/HomeController.cs
@@ -1,100 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using sistemanomina.Models;
+using System;
+using System.Data.Entity;
+using System.Data.SqlClient;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using sistemanomina.Models; // Asegúrate de que este espacio de nombres sea correcto
-using System.Data.Entity;   // para incluir EF
 
 namespace sistemanomina.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
+        public ActionResult Index() => View();
+        public ActionResult About() { ViewBag.Message = "Your application description page."; return View(); }
+        public ActionResult Contact() { ViewBag.Message = "Your contact page."; return View(); }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+        public ActionResult Autenticacion() { ViewBag.Message = "Pagina de Autenticacion"; return View(); }
+        public ActionResult Empleados() { ViewBag.Message = "Pagina de Empleados"; return View(); }
+        public ActionResult Departamentos() { ViewBag.Message = "Pagina de Departamentos"; return View(); }
+        public ActionResult AsignacionADepartamentos() { ViewBag.Message = "Pagina de asignacion a departamentos"; return View(); }
+        public ActionResult GerentesDeDepartamento() { ViewBag.Message = "Pagina de los Gerentes de departamento"; return View(); }
+        public ActionResult TitulosCargos() { ViewBag.Message = "Pagina de los ttulos / Cargos"; return View(); }
+        public ActionResult Salarios() { ViewBag.Message = "Pagina de Salarios"; return View(); }
+        public ActionResult Reportes() { ViewBag.Message = "Pagina de los Reportes"; return View(); }
+        public ActionResult Administracion() { ViewBag.Message = "Pagina de Administracion"; return View(); }
 
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
-        // Aqui es todo lo creado
-        public ActionResult Autenticacion()
-        {
-            ViewBag.Message = "Pagina de Autenticacion";
-            return View();
-        }
-        public ActionResult Empleados()
-        {
-            ViewBag.Message = "Pagina de Empleados";
-            return View();
-        }
-        public ActionResult Departamentos()
-        {
-            ViewBag.Message = "Pagina de Departamentos";
-            return View();
-        }
-        public ActionResult AsignacionADepartamentos()
-        {
-            ViewBag.Message = "Pagina de asignacion a departamentos";
-            return View();
-        }
-        public ActionResult GerentesDeDepartamento()
-        {
-            ViewBag.Message = "Pagina de los Gerentes de departamento";
-            return View();
-
-        
-        }
-        public ActionResult TitulosCargos()
-        {
-            ViewBag.Message = "Pagina de los ttulos / Cargos";
-            return View();
-
-
-        }
-        public ActionResult Salarios()
-        {
-            ViewBag.Message = "Pagina de Salarios";
-            return View();
-
-
-        }
-        public ActionResult Reportes()
-        {
-            ViewBag.Message = "Pagina de los Reportes";
-            return View();
-
-
-        }
-        public ActionResult Administracion()
-        {
-            ViewBag.Message = "Pagina de Administracion";
-            return View();
-
-
-        }
         public ActionResult ProbarConexion()
         {
             using (var db = new NominaContext())
             {
-                var count = db.Empleados.Count(); // Esto fuerza la creación de la DB
                 try
                 {
-                    
-                    // Cuenta los empleados de la tabla
-
                     int cantidad = db.Empleados.Count();
                     ViewBag.Message = "Conexión exitosa. Empleados registrados: " + cantidad;
                 }
@@ -103,9 +37,51 @@ namespace sistemanomina.Controllers
                     ViewBag.Message = "Error en la conexión: " + ex.Message;
                 }
             }
-
             return View();
+        }
 
+        // ========================================
+        // Acción para mostrar detalle del empleado
+        // ========================================
+        public ActionResult DetalleEmpleado(int id)
+        {
+            var empleado = ObtenerEmpleadoPorId(id);
+
+            if (empleado == null)
+                return HttpNotFound();
+
+            return View(empleado);
+        }
+
+        // ========================================
+        // Método que llama al SP usando EF6
+        // ========================================
+        
+        /*
+         * private Empleado ObtenerEmpleadoPorId(int id)
+        {
+            using (var context = new NominaContext())
+            {
+                var empleado = context.Database.SqlQuery<Empleado>(
+                    "EXEC sp_ObtenerEmpleadoPorID @Id = {1}", id
+                ).FirstOrDefault();
+
+                return empleado;
+            }
+        }
+        */
+        private Empleado ObtenerEmpleadoPorId(int id)
+        {
+            using (var context = new NominaContext())
+            {
+                var param = new SqlParameter("@Id", id);
+
+                var empleado = context.Database.SqlQuery<Empleado>(
+                    "EXEC sp_ObtenerEmpleadoPorID @Id", param
+                ).FirstOrDefault();
+
+                return empleado;
+            }
         }
     }
 }
